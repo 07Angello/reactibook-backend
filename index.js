a6dd7d69f0ea6c522a12b5066cf6c071285ed94a
Angello Antonelly <angelloantonelly@live.com>
2021-02-05 22:53:05 -0600

    restored the index.js

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-const express = require('express');
-const { dbConnection } = require('./config/database');
-const app = express();
-require('dotenv').config();
-const cors = require('cors');
-const routes = require('./routes/routes');
-
-dbConnection();
-
-app.use( cors() );
-app.use( express.static('public') );
-app.use( express.json() );
-
-app.use( routes );
+const app = require('./app');
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
