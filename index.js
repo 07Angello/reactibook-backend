aad6108ef154a67e1f92d0396ce08065ca14cf67
Angello Antonelly <angelloantonelly@live.com>
2021-02-05 22:49:26 -0600

    Testing deployment

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const app = require('./app');
+const express = require('express');
+const { dbConnection } = require('./config/database');
+const app = express();
+require('dotenv').config();
+const cors = require('cors');
+const routes = require('./routes/routes');
+
+dbConnection();
+
+app.use( cors() );
+app.use( express.static('public') );
+app.use( express.json() );
+
+app.use( routes );
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
